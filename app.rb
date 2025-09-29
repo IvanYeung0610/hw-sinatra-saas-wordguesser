70a1021e1316454881f96f04bc02b22ee4b3195b
Ivan Yeung <iy2112@nyu.edu>
2025-09-29 05:26:04 +0000

    Fixed missing period in message.

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ class WordGuesserApp < Sinatra::Base
     end
 
     if res == false
-      flash[:message] = "You have already used that letter"
+      flash[:message] = "You have already used that letter."
     end
     
     redirect '/show'
